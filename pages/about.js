63be74329156d73376c9bca59580d79716ef0e9c
INnoVatioN-97 <koyoungil97@gmail.com>
2022-02-20 17:34:10 +0900

    Routing

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,12 @@
 // 아무런 route 설정을 안해줬는데도 그냥 파일명대로 /about만 치니까
 // 이 페이지가 나옴. 신기방기.
 
+import { NavBar } from '../components/NavBar';
+
 export default function Potato() {
 	return (
 		<div>
+			<NavBar />
 			<h1>about us</h1>
 		</div>
 	);
